8f8600a1b6ce17fb7e1fb6a5f582d457a9aac792
Alex Starovoitov <star@n1.by>
2014-04-09 10:10:14 +0300

    add ImageMapper

---
+++ MultilayerPerceptron/ImageMapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilayerPerceptron
+{
+    public class ImageMapper
+    {
+        public virtual double[] ToDouble(Bitmap image)
+        {
+            var result = new double[image.Height * image.Width];
+
+            for (int x = 0; x < image.Height; x++)
+            {
+                for (int y = 0; y < image.Width; y++)
+                {
+                    result[x*image.Height + y] = ActivationFunction(image.GetPixel(x, y));
+                }
+            }
+
+            return result;
+        }
+
+        private double ActivationFunction(Color pixel)
+        {
+			if (pixel.B > 127 && pixel.R > 127 && pixel.G > 127)
+			{
+				return 1;
+			}
+
+			return 0;
+		}
+        
+    }
+}
